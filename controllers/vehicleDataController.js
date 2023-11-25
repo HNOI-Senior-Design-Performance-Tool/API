6d68d743bc90b2daf5cb8fbfea8a50bf5926a621
CameronSettles <46801236+CameronSettles@users.noreply.github.com>
2023-11-24 23:14:49 -0800

    Replaced monthly averager

--- controllers/vehicleDataController.js
+++ controllers/vehicleDataController.js
@@ -15,7 +15,7 @@ const uploadVehicleData = async (req, res) => {
             NOx, 
             particulateMatter, 
             fuelLevel, 
-            voltage, 
+            voltage,
             time,
         })
         res.status(200).json(vehicleData)
@@ -250,74 +250,6 @@ const deleteAllData = async (req, res) => {
     }
 }
 
-<<<<<<< HEAD
-=======
-// Calculate and post the average data for the current month
-const getAverageMonthlyData = async (req, res) => {
-    // const currentDate = new Date()
-    // const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1, 0, 0, 0)
-    // const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0, 23, 59, 59)
-
-    const { startOfMonth, endOfMonth } = req.params
-
-    const vehicleData = await VehicleData.find({
-        time: {
-            $gte: startOfMonth, 
-            $lte: endOfMonth    
-        }
-    })
-
-    if (!vehicleData || vehicleData.length === 0) {
-        return res.status(404).json({ error: 'No data within the range' })
-    }
-
-    // Initialize objects to store the sum of each field and count of data points
-    const sum = {
-        mpg: 0,
-        CO: 0,
-        NOx: 0,
-        particulateMatter: 0,
-        fuelLevel: 0,
-        flowrate: 0,
-    };
-    let count = 0
-
-    // Calculate the sum of each field and count of data points
-    vehicleData.forEach(dataPoint => {
-        // Check if the field is valid
-        if (dataPoint.mpg !== undefined) sum.mpg += dataPoint.mpg;
-        if (dataPoint.CO !== undefined) sum.CO += dataPoint.CO;
-        if (dataPoint.NOx !== undefined) sum.NOx += dataPoint.NOx;
-        if (dataPoint.particulateMatter !== undefined) sum.particulateMatter += dataPoint.particulateMatter;
-        if (dataPoint.fuelLevel !== undefined) sum.fuelLevel += dataPoint.fuelLevel;
-        if (dataPoint.flowrate !== undefined) sum.flowrate += dataPoint.flowrate;
-        count++;
-    })
-
-    // Calculate the average of each field
-    const average = {}
-    for (const key of Object.keys(sum)) {
-        average[key] = sum[key] / count
-    }
-
-    // Create a new document in the database to store the average values
-    const averageData = await AvgData.create({
-        startRange: startOfMonth,
-        endRange: endOfMonth,
-        mpg: average.mpg,
-        CO: average.CO,
-        NOx: average.NOx,
-        particulateMatter: average.particulateMatter,
-        fuelLevel: average.fuelLevel,
-        flowrate: average.flowrate,
-        time: new Date(),
-    })
-
-    res.status(200).json({ averageData })
-}
-
-
->>>>>>> 17ac062b50832f0b720d12d7aa163545ecbd631e
 module.exports = {
   uploadVehicleData,
   getAllData,
