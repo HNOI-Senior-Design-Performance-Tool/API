cf70db42e2006cdf93861596cb3fc060938c72c5
CameronSettles <camsettles662@gmail.com>
2023-12-10 14:50:04 -0800

    Fix bug in login functionality

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.use(
     })
 );
 
+// Add headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin to access the server
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Allow these HTTP methods
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // Allow these headers
+  res.setHeader('Access-Control-Allow-Credentials', true); // Allow cookies
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 /*
